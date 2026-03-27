9b4c23a6c441d8036f65677b768dd2a1110fb7c4
Rumonge Time <rumongetime@gmail.com>
2026-03-27 08:08:51 +0000

    Sorry, I hit a snag. Please try again shortly or modify your prompt. [G

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-4089773393-16068",
-  "appId": "1:647999302804:web:a8735ac6c5752558a61881",
-  "apiKey": "AIzaSyBedrj4LxSm1Tf7unTGHwPJbotWFJ5MGBk",
-  "authDomain": "studio-4089773393-16068.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "647999302804"
+  apiKey: "AIzaSyBedrj4LxSm1Tf7unTGHwPJbotWFJ5MGBk",
+  authDomain: "studio-4089773393-16068.firebaseapp.com",
+  projectId: "studio-4089773393-16068",
+  storageBucket: "studio-4089773393-16068.firebasestorage.app",
+  messagingSenderId: "647999302804",
+  appId: "1:647999302804:web:a8735ac6c5752558a61881"
 };
